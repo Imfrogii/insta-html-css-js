2cd098df2ebc0f9dace0fc19ed83b9fc9a117e27
Imfrogii <vip.shelko@mail.ru>
2019-05-11 01:44:26 +0300

    add servlets

--- UI_last_version/script.js
+++ UI_last_version/script.js
@@ -1,188 +1,32 @@
 let photoPosts = [{
     id: '1',
     descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-    createdAt: new Date('2018-06-03T23:00:00'),
-    author: 'User1',
+    createdAt: new Date('2018-02-23T23:00:00'),
+    author: 'aa2',
     photoLink: 'E:/web/img/photo1',
     hashtags: ["#ку", "#привет", "#ао"],
-    likes: ["nnds", "dfg", "yyeh", "jsdfg"],
+    likes: 160,
   },
   {
     id: '2',
     descriprion: 'Красота какая',
-    createdAt: new Date('2018-02-11T23:00:00'),
-    author: 'ALek',
+    createdAt: new Date('2018-02-23T23:00:00'),
+    author: 'aa1',
     photoLink: 'E:/web/img/photo2',
-    hashtags: ["#hi"],
-    likes: ["2TTs", "lhdf", "tyho", "11G"],
+    hashtags:[],
+    likes: 12,
   },
   {
     id: '3',
-    descriprion: 'Красота какая[2]',
-    createdAt: new Date('2018-03-23T23:00:00'),
-    author: 'AjsS',
+    descriprion: 'Красота какая',
+    createdAt: new Date('2018-02-23T23:00:00'),
+    author: 'aa1',
     photoLink: 'E:/web/img/photo3',
-    hashtags: ["#ку"],
-    likes: ["TTs", "lhdf", "yyeh", "jsdfg"],
-  },
-  {
-    id: '4',
-    descriprion: 'Добавил новую фотку',
-    createdAt: new Date('2018-02-20T23:00:00'),
-    author: 'Man',
-    photoLink: 'E:/web/img/photo2',
-    hashtags: ["#hi", "#add"],
-    likes: [],
-  },
-  {
-    id: '5',
-    descriprion: 'Женщины - крута',
-    createdAt: new Date('2018-12-12T23:00:00'),
-    author: 'Women',
-    photoLink: 'E:/web/img/photo3',
-    hashtags: ["#ку"],
-    likes: ["JSa"],
-  },
-  {
-    id: '6',
-    descriprion: 'иду в школу',
-    createdAt: new Date('2018-02-04T23:00:00'),
-    author: 'Child112',
-    photoLink: 'E:/web/img/photo2',
-    hashtags: [],
-    likes: ["john", "YYf66", "Aleks", "Fedor", "JSa"],
-  },
-  {
-    id: '7',
-    descriprion: 'А как ваши дела',
-    createdAt: new Date('2018-02-02T23:00:00'),
-    author: 'Find_Me',
-    photoLink: 'E:/web/img/photo3',
-    hashtags: ["#ку"],
-    likes: ["Aleks", "Fedor", "JSa"],
-  },
-  {
-    id: '8',
-    descriprion: 'Красиво',
-    createdAt: new Date('2018-09-11T23:00:00'),
-    author: 'Find_Me',
-    photoLink: 'E:/web/img/photo3',
-    hashtags: ["#ку", "#yyeh"],
-    likes: ["Aleks", "Fedor", "JSa", "112", "Boy228"],
-  },
-  {
-    id: '9',
-    descriprion: 'Уехать бы сюда',
-    createdAt: new Date('2018-12-10T23:00:00'),
-    author: 'Bolll',
-    photoLink: 'E:/web/img/photo3',
-    hashtags: ["#krasota"],
-    likes: ["Aleks", "Fedor", "JSa"],
-  },
-  {
-    id: '10',
-    descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-    createdAt: new Date('2018-10-03T23:00:00'),
-    author: 'Admin',
-    photoLink: 'E:/web/img/photo1',
-    hashtags: ["#ку", "#привет", "#TOOOOp"],
-    likes: ["User1", "dfg", "yyeh", "jsdfg"],
+    hashtags:["#ку"],
+    likes: 125,
   },
 
-  {
-    id: '11',
-    descriprion: 'Cборная Беларуси выиграла эстафету в Пхёнчхане!!!',
-    createdAt: new Date('2018-09-09T23:00:00'),
-    author: 'User1',
-    photoLink: 'E:/web/img/photo1',
-    hashtags: ["#sport", "#belarus", "#ао"],
-    likes: ["nnds"],
-  },
-  {
-    id: '12',
-    descriprion: 'Работаю',
-    createdAt: new Date('2018-03-03T23:00:00'),
-    author: 'Орк',
-    photoLink: 'E:/web/img/photo1',
-    hashtags: ["#жизнь_за_нерзулла", "#построить", "#радость"],
-    likes: ['Орк[1]', "Орк[2]"],
-  },
-  {
-    id: '13',
-    descriprion: 'Поздравляем наших девочек',
-    createdAt: new Date('2018-01-05T23:00:00'),
-    author: 'admin22',
-    photoLink: 'E:/web/img/photo1',
-    hashtags: ["#8марта", "#девочки", "#праздник"],
-    likes: ["yyeh", "jsdfg"],
-  },
-  {
-    id: '14',
-    descriprion: 'Поздравляем нас',
-    createdAt: new Date('2018-05-02T23:00:00'),
-    author: 'ТвояМама',
-    photoLink: 'E:/web/img/photo1',
-    hashtags: [],
-    likes: [],
-  },
-
-  {
-    id: '15',
-    descriprion: 'С 8 МАРТА!!!',
-    createdAt: new Date('2018-06-09T23:00:00'),
-    author: 'Девочка',
-    photoLink: 'E:/web/img/photo1',
-    hashtags: [],
-    likes: ["Aleks", "Fedor", "JSa", "112", "Boy228"],
-  },
-
-  {
-    id: '16',
-    descriprion: 'мне подарили цветы дадада',
-    createdAt: new Date('2018-01-03T23:00:00'),
-    author: 'girl1',
-    photoLink: 'E:/web/img/photo1',
-    hashtags: ["#цветы", "#жизнб", "#радость"],
-    likes: [],
-  },
-  {
-    id: '17',
-    descriprion: 'за Орудууу',
-    createdAt: new Date('2018-16-03T23:00:00'),
-    author: 'Орк',
-    photoLink: 'E:/web/img/photo1',
-    hashtags: ["#жизнь_за_нерзулла", "#построить", "#радость"],
-    likes: ['OldFag', 'NewFag', 'SonO'],
-  },
-  {
-    id: '18',
-    descriprion: 'за Орудууу[2]',
-    createdAt: new Date('2018-02-03T23:00:00'),
-    author: 'Орк[2]',
-    photoLink: 'E:/web/img/photo1',
-    hashtags: ["#жизнь_за_нерзулла[2]", "#построить", "#радость"],
-    likes: ['OldFag', 'NewFag'],
-  },
-  {
-    id: '19',
-    descriprion: 'за Орудууу[3]',
-    createdAt: new Date('2018-06-04T23:00:00'),
-    author: 'Орк[3]',
-    photoLink: 'E:/web/img/photo1',
-    hashtags: ["#жизнь_за_нерзулла", "#построить", "#работа-радость"],
-    likes: ['OldFag', 'NewFag'],
-  },
-  {
-    id: '20',
-    descriprion: 'Урааа',
-    createdAt: new Date('2018-06-05T23:00:00'),
-    author: 'Aleks',
-    photoLink: 'E:/web/img/photo1',
-    hashtags: ["#жизнь_за_нерзулла", "#построить", "#радость"],
-    likes: ['Aleks'],
-  },
 ];
-
 
 function getAuthor(id) {
   for (let i = 0; i < photoPosts.length; i++) {
@@ -220,335 +64,79 @@ function getImg(id) {
   }
 };
 
-String.prototype.replaceAll = function(search, replace) {
-  return this.split(search).join(replace);
+String.prototype.replaceAll = function(search, replace){
+return this.split(search).join(replace);
 }
 
-let sortDates = function compare(a, b) {
-  return b.createdAt - a.createdAt;
-}
+function getPhotoPosts(skip, top, ...filter) {
+    if(filter.length>0){
+    filteredArr = findFilter(...filter);
+    for(let i = 0; i<filteredArr.length; i++){
 
-function getPhotoPosts(skip, top, filter) {
-  let answer = [];
-  skip = skip || 0;
-  top = top || 10;
-  if (filter != undefined) {
-    let filteredArr = findFilter(filter);
-
-    if (skip > filteredArr.length) {
-      return 0;
     }
-    answer = filteredArr.slice(skip, top);
-  } else {
-    answer = photoPosts.slice(skip, top + skip);
   }
-
-  return answer.sort(sortDates);
 }
 
-
-function findFilter(filter) {
+function findFilter(...filter) {
+  let n = arguments.length;
+  let arrOfLex = "";
+  let resArr = "";
   let endArr = [];
-  let count = 0;
-  for (countOfFilter in filter) {
-    count++;
-  }
-  let mapForChoose = new Map();
-  if (filter.author != undefined) {
-    for (let item of photoPosts) {
-      if (item.author === filter.author) {
-        // arrFilter.push(item);
-        if (mapForChoose.has(item)) { //ДЛЯ ПОДСЧЕТА КОЛИЧЕСТВА РАЗ КОГДА
-          // НАШЕЛСЯ ТАКОЙ ПОСТ И ДЛЯ ВЫВОДА ТОЛЬКО ТЕХ,
-          //У КОТОРЫХ БУДУТ ВЫПОЛНЯТСЯ ВСЕ УСЛОВИЯ ФИЛЬТРА
-          mapForChoose.set(item, mapForChoose.get(item) + 1)
-        } else mapForChoose.set(item, 1);
-      }
-    }
-  }
+  for (let i = 0; i < filter.length; i++) {
+    filter[i] = filter[i].toString();
+    arrOfLex = filter[i].split(":")
+    console.log(arrOfLex);
 
-  if (filter.hashtags != undefined) {
-    for (let item of photoPosts) {
-      let countTags = 0;
-      for (let i = 0; i < item.hashtags.length; i++) {
-        for (let j = 0; j < filter.hashtags.length; j++) {
-          if (item.hashtags[i].toLowerCase() === filter.hashtags[j].toLowerCase()) {
-            countTags++;
+    switch (arrOfLex[0]) {
+      case "author":
+        arrOfLex[1] = arrOfLex[1].replace(/" "/g, "");
+        for (let j = 0; j < photoPosts.length; j++) {
+          if (photoPosts[j].author == arrOfLex[1]) {
+            resArr += String(j);
+            resArr += " ";
           }
         }
-      }
-      if (countTags >= filter.hashtags.length) {
-        if (mapForChoose.has(item)) {
-          mapForChoose.set(item, mapForChoose.get(item) + 1)
-        } else mapForChoose.set(item, 1);
-      }
-    }
-  }
+        break;
+      //  console.log(resArr);
 
-  if (filter.likes != undefined) {
-    for (let item of photoPosts) {
-      for (let i = 0; i < item.likes.length; i++) {
-        if (item.likes[i].toLowerCase() === filter.likes.toLowerCase()) {
-          // arrFilter.push(item);
-          if (mapForChoose.has(item)) {
-            mapForChoose.set(item, mapForChoose.get(item) + 1)
-          } else mapForChoose.set(item, 1);
+      case "createdAt":
+        arrOfLex[1] = arrOfLex[1].replace(/" "/g, "");
+        for (let j = 0; j < photoPosts.length; j++) {
+          if (photoPosts[j].createdAt == arrOfLex[1]) {
+            resArr += String(j);
+            resArr += " ";
+          }
         }
-      }
+        break;
+
+      case "hashtags":
+        newArr = arrOfLex[1].split(" ");
+        for (let j = 0; j < photoPosts.length; j++) {
+          for (let k = 0; k < photoPosts[j].hashtags.length; k++) {
+            for (let m = 0; m < newArr.length; m++) {
+              if (photoPosts[j].hashtags[k].toLowerCase() == newArr[m].toLowerCase()) {
+                resArr += String(j);
+                resArr += " ";
+                //console.log(resArr);
+              }
+            }
+          }
+        }
+        break;
     }
   }
+  var temp = resArr;
+  while (resArr != "") {
+    if (resArr[0] != " ") {
 
-  for (let item of mapForChoose) {
-    if (item[1] >= count) {
-      endArr.push(item[0]);
-    }
+      if (temp.length - (resArr = resArr.replaceAll(resArr[0], "")).length == n) {//////////вот тут не проходит 2 элемент
+        endArr.push(temp[0]);
+      }
+        temp = resArr;
+
+    } else resArr = resArr.replace(resArr[0], "");
   }
   return endArr;
 }
 
-function getPhotoPost(id) {
-  for (let item of photoPosts) {
-    if (item.id === id) {
-      return item;
-    }
-  }
-  return false;
-}
-
-
-function validatePhotoPost(post) {
-  if (post.id != undefined &&
-    post.createdAt != undefined &&
-    post.descriprion != undefined &&
-    post.descriprion.length <= 200 &&
-    post.descriprion.length > 0 &&
-    post.author != undefined &&
-    post.photoLink != undefined) {
-
-    if ((typeof(post.id) == "string") &&
-      (typeof(post.descriprion) == "string") &&
-      (typeof(post.author) == "string") &&
-      (typeof(post.photoLink) == "string") &&
-      (post.createdAt instanceof Date)) {
-
-      if ((Array.isArray(post.likes)) &&
-        (Array.isArray(post.hashtags))) {
-        return true;
-      } else {
-        return false;
-      }
-      return true;
-    } else {
-      return false;
-    }
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function addPhotoPost(photoPost) {
-  photoPost.id = ((parseInt((photoPosts[photoPosts.length - 1].id)) + 1) + "");
-  photoPost.createdAt = new Date();
-  photoPost.likes = [];
-  if (photoPost.hashtags === undefined) {
-    photoPost.hashtags = [];
-  }
-
-  if (validatePhotoPost(photoPost)) {
-    photoPosts.push(photoPost);
-    console.log(photoPost);
-    console.log(photoPosts);
-    return true;
-  } else return false;
-}
-
-
-function editPhotoPost(id, photoPost) {
-  let i = 0;
-  for (let item of photoPosts) {
-    if (item.id === id) {
-      photoPost.id = item.id;
-      photoPost.createdAt = item.createdAt;
-      photoPost.likes = item.likes;
-      photoPost.author = item.author;
-      if (photoPost.hashtags === undefined) {
-        photoPost.hashtags = item.hashtags;
-      }
-      if (photoPost.photoLink === undefined) {
-        photoPost.photoLink = item.photoLink;
-      }
-      if (photoPost.descriprion === undefined) {
-        photoPost.descriprion = item.descriprion;
-      }
-      if (validatePhotoPost(photoPost)) {
-        photoPosts[i] = photoPost;
-        console.log(photoPost);
-        console.log(photoPosts);
-        return true;
-      } else return false;
-    }
-    i++;
-  }
-  return false;
-}
-
-function removePhotoPost(id) {
-  let i = 0;
-  for (let item of photoPosts) {
-    if (item.id === id) {
-      delete photoPosts[i];
-      photoPosts.clean(undefined);
-      console.log(photoPosts);      
-      return true;
-    }
-    i++;
-  }
-  return false;
-}
-
-Array.prototype.clean = function(deleteValue) {
-  for (var i = 0; i < this.length; i++) {
-    if (this[i] == deleteValue) {
-      this.splice(i, 1);
-      i--;
-    }
-  }
-  return this;
-};
-
-console.log('getPhotoPost(\'1\')');
-
-console.log(getPhotoPost('1'));
-
-
-
-console.log('getPhotoPost(\'sdadsasdadsa\')');
-
-console.log(getPhotoPost('sdadsasdadsa'));
-
-
-
-console.log('getPhotoPost(\'123123\')');
-
-console.log(getPhotoPost('123123'));
-
-
-
-console.log('getPhotoPost(\'{a:\'fdf\'}}\')');
-
-console.log(getPhotoPost({a:'fdf'}));
-
-
-
-console.log('getPhotoPost()');
-
-console.log(getPhotoPosts());
-
-console.log('getPhotoPosts(10, 10)');
-
-console.log(getPhotoPosts(10, 10));
-
-
-
-console.log('getPhotoPosts(0, 10, {author: \'Орк\'})');
-
-console.log(getPhotoPosts(0, 10, {author: 'Орк'}))
-
-
-
-console.log('getPhotoPosts(0, 10, {author: \'Орк\', hashtags: [\'#радость\']})');
-
-console.log(getPhotoPosts(0, 10, {author: 'Орк', hashtags: ['#радость']}));
-
-
-
-console.log('getPhotoPosts(0, 10, {author: \'Орк\', hashtags: [\'#радость\', \'#caTS\']})');
-
-console.log(getPhotoPosts(0, 10, {author: 'Орк', hashtags: ['#радость', '#caTS']}));
-
-
-
-console.log('addPhotoPost({descriprion: \'Hello, 16\', photoLink: \'images/example16\', hashtags:[\'#JS\']})');
-
-console.log(addPhotoPost({descriprion: 'Hello, 16', photoLink: 'images/example16', hashtags:['#JS']}));
-
-
-
-console.log('addPhotoPost({descriprion: \'Hello, 16\',author: \'WindowsUser\', hashtags:[\'#JS\']})');
-
-console.log(addPhotoPost({descriprion: 'Hello, 16',author: 'WindowsUser', hashtags:['#JS']}));
-
-
-
-console.log('addPhotoPost({descriprion: \'Hello, 16\',author: \'WindowsUser\',photoLink: \'images/example16\', hashtags:[\'JS\']})');
-
-console.log(addPhotoPost({descriprion: 'Hello, 16',author: 'WindowsUser',photoLink: 'images/example16', hashtags:['#JS']}));
-
-
-
-console.log('getPhotoPost(21)');
-
-console.log(getPhotoPost('21'));
-
-
-
-console.log('removePhotoPost(\'21\')');
-
-console.log(removePhotoPost('21'));
-
-
-
-console.log('getPhotoPosts(0,20)');
-
-console.log(getPhotoPosts(0,20));
-
-
-
-console.log('editPhotoPost(\'1\',{photoLink: [\'Hello\', \'Andrey\']})');
-
-console.log(editPhotoPost('1',{photoLink: ['Hello', 'Andrey']}));
-
-
-
-
-
-
-console.log('editPhotoPost(\'1\',{descriprion: \'Hello JS\' ,hashtags:[\'#Hello\', \'#JS\']})');
-
-console.log(editPhotoPost('1',{descriprion: 'Hello JS',hashtags:['#Hello', '#JS']}));
-
-
-
-console.log('getPhotoPost(1)');
-
-console.log(getPhotoPost('1'));
-
-
-
-console.log('editPhotoPost(\'1\',{photoLink: \'images/Hello_JS.jpg\'})');
-
-console.log(editPhotoPost('1',{photoLink: 'images/Hello_JS.jpg'}));
-
-
-
-console.log('getPhotoPost(1)');
-
-console.log(getPhotoPost('1'));
-
-
-
-console.log('removePhotoPost(\'1\')');
-
-console.log(removePhotoPost('1'));
-
-
-console.log('removePhotoPost(\'12\')');
-
-console.log(removePhotoPost('12'));
-
-
-
-console.log('removePhotoPost(\'JS\')');
-
-console.log(removePhotoPost('JS'));
+  console.log(getAuthor(2), getDate(2));
